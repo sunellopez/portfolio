c329b47234e451e9b67089e526309e39073f623f
sunellopez <sunellopez@gmail.com>
2023-12-26 01:03:47 -0600

    first commiit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import './assets/main.css'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, fab)
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
